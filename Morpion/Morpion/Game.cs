c32343fbea44b48749717e45694a6c642e63ad88
march67 <td.ta.david@gmail.com>
2025-12-11 15:29:48 +0100

    feature: Can now play vs a bot Can choose the type of game to play, either vs another human or vs a bot refactor: IPlayer.cs - BotPlayer.cs - HumanPlayer.cs instead of a single type of Player refactor: Input (player move) extracted away from Board.cs to IPlayer

--- Morpion/Morpion/Game.cs
+++ Morpion/Morpion/Game.cs
@@ -8,12 +8,12 @@ namespace Morpion
 {
     public class Game
     {
-        List<Player> playerList = new List<Player>();
+        List<IPlayer> playerList = new List<IPlayer>();
         bool Restart = false;
 
         public void StartGame()
         {
-            InitializeTwoPlayers();
+            ChooseTypeOfGame();
 
             Console.Clear();
 
@@ -22,7 +22,29 @@ namespace Morpion
             GameEnded();
         }
 
-        public void InitializeTwoPlayers()
+        private void ChooseTypeOfGame()
+        {
+            char? input;
+            do
+            {
+                Console.Write("Choisissez '1' pour jouer contre un autre humain, '2' pour jouer contre un bot\n");
+                input = Console.ReadLine()[0];
+
+
+            } while (input != '1' && input != '2');
+
+            if (input == '1')
+            {
+                InitializeTwoHumanPlayers();
+            }
+            else if (input == '2')
+            {
+                InitializeHumanVsBotPlayers();
+            }
+
+        }
+
+        public void InitializeTwoHumanPlayers()
         {
             string? input;
 
@@ -58,8 +80,39 @@ namespace Morpion
             } while (string.IsNullOrEmpty(input));
             char playerSymbol_2 = input[0];
 
-            playerList.Add(new Player { PlayerName = playerName_1, PlayerSymbol = playerSymbol_1 });
-            playerList.Add(new Player { PlayerName = playerName_2, PlayerSymbol = playerSymbol_2 });
+            playerList.Add(new HumanPlayer { HumanName = playerName_1, HumanSymbol = playerSymbol_1 });
+            playerList.Add(new HumanPlayer { HumanName = playerName_2, HumanSymbol = playerSymbol_2 });
+        }
+
+        public void InitializeHumanVsBotPlayers()
+        {
+            string? input;
+
+
+            Console.Write("Joueur 1, veuillez saisir votre prénom\n");
+            do
+            {
+                input = Console.ReadLine();
+            } while (string.IsNullOrEmpty(input));
+            string playerName_1 = input;
+
+            Console.Write("Joueur 1, veuillez saisir votre symbole de jeu\n");
+            do
+            {
+                input = Console.ReadLine();
+
+            } while (string.IsNullOrEmpty(input));
+            char playerSymbol_1 = input[0];
+
+            Random random = new Random();
+            string[] botNames = { "Skynet", "HAL-9000", "Cortana", "GLaDOS", "R2D2", "Wall-E" };
+            char[] availableSymbols = { 'X', 'O', '#', '@', '*', '&' };
+
+            string botName = botNames[random.Next(botNames.Length)]; // Next génère un entier aléatoire compris entre 0 et la maxValue en argument
+            char botSymbol = availableSymbols[random.Next(availableSymbols.Length)];
+
+            playerList.Add(new HumanPlayer { HumanName = playerName_1, HumanSymbol = playerSymbol_1 });
+            playerList.Add(new BotPlayer { BotName = botName, BotSymbol  = botSymbol });
         }
 
         public void GameEnded()
---
+++ Morpion/Morpion/IPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morpion
+{
+    public interface IPlayer
+    {
+        bool CheckPlayerName();
+
+        bool CheckPlayerSymbol();
+
+        string GetPlayerName();
+
+        char GetPlayerSymbol();
+
+        void PlayerInput(Board board);
+    }
+}
